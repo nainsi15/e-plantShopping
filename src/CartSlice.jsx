8af66cdb15c8cba990554f3a3c94b11f19756e8b
nainsi15i <2306340@kiit.ac.in>
2026-06-17 16:26:10 -0400

    Completed CartSlice reducers

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -6,17 +6,37 @@ export const CartSlice = createSlice({
     items: [], // Initialize items as an empty array
   },
   reducers: {
-    addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+  addItem: (state, action) => {
+    const item = state.items.find(
+      (item) => item.name === action.payload.name
+    );
 
-    
-    },
+    if (item) {
+      item.quantity += 1;
+    } else {
+      state.items.push({
+        ...action.payload,
+        quantity: 1,
+      });
+    }
   },
-});
+
+  removeItem: (state, action) => {
+    state.items = state.items.filter(
+      (item) => item.name !== action.payload.name
+    );
+  },
+
+  updateQuantity: (state, action) => {
+    const item = state.items.find(
+      (item) => item.name === action.payload.name
+    );
+
+    if (item) {
+      item.quantity = action.payload.quantity;
+    }
+  },
+},
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
