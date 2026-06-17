5a444df71808c65381719ceb02f1a61c965d888e
nainsi15i <2306340@kiit.ac.in>
2026-06-17 16:58:09 -0400

    Fixed ProductList and CartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,40 +3,41 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
   },
   reducers: {
-  addItem: (state, action) => {
-    const item = state.items.find(
-      (item) => item.name === action.payload.name
-    );
+    addItem: (state, action) => {
+      const item = state.items.find(
+        (item) => item.name === action.payload.name
+      );
 
-    if (item) {
-      item.quantity += 1;
-    } else {
-      state.items.push({
-        ...action.payload,
-        quantity: 1,
-      });
-    }
+      if (item) {
+        item.quantity += 1;
+      } else {
+        state.items.push({
+          ...action.payload,
+          quantity: 1,
+        });
+      }
+    },
+
+    removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload.name
+      );
+    },
+
+    updateQuantity: (state, action) => {
+      const item = state.items.find(
+        (item) => item.name === action.payload.name
+      );
+
+      if (item) {
+        item.quantity = action.payload.quantity;
+      }
+    },
   },
-
-  removeItem: (state, action) => {
-    state.items = state.items.filter(
-      (item) => item.name !== action.payload.name
-    );
-  },
-
-  updateQuantity: (state, action) => {
-    const item = state.items.find(
-      (item) => item.name === action.payload.name
-    );
-
-    if (item) {
-      item.quantity = action.payload.quantity;
-    }
-  },
-},
+});
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
